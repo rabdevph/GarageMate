59e46670a5db372ae4a279db65bbb982a3ef8065
Ralph Arsy Bauto <rab.devph@gmail.com>
2025-06-08 23:16:07 +0400

    feat(config): load PostgreSQL connection string from environment variables

--- GarageMate.Api/Program.cs
+++ GarageMate.Api/Program.cs
@@ -1,4 +1,19 @@
+using GarageMate.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var host = Environment.GetEnvironmentVariable("PG_HOST");
+var port = Environment.GetEnvironmentVariable("PG_PORT");
+var db = Environment.GetEnvironmentVariable("PG_DB");
+var user = Environment.GetEnvironmentVariable("PG_USER");
+var password = Environment.GetEnvironmentVariable("PG_PASSWORD");
+
+var connectionString = $"Host={host};Port={port};Database={db};Username={user};Password={password}";
+
+builder.Services.AddDbContext<GarageMateContext>(options =>
+    options.UseNpgsql(connectionString));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello GarageMate!");
