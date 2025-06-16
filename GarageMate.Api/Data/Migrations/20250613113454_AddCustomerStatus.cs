8f26b94f60d0ab20887853bdccde178b4aa43ad2
Ralph Arsy Bauto <rab.devph@gmail.com>
2025-06-16 18:23:21 +0400

    chore(migrations): add Status property to Customer entity

---
+++ GarageMate.Api/Data/Migrations/20250613113454_AddCustomerStatus.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GarageMate.Api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCustomerStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Customers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Customers");
+        }
+    }
+}
