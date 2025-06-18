40a522344f4ee7bd4db75dbf4d7f5cb07f2119d6
Ralph Arsy Bauto <rab.devph@gmail.com>
2025-06-18 23:10:26 +0400

    feat(endpoints): enhance customer creation with validation and error handling

--- GarageMate.Api/Endpoints/CustomerEndpoints.cs
+++ GarageMate.Api/Endpoints/CustomerEndpoints.cs
@@ -2,6 +2,7 @@ using GarageMate.Api.Data;
 using GarageMate.Api.Dtos.Customers;
 using GarageMate.Api.Enums;
 using GarageMate.Api.Extensions;
+using GarageMate.Api.Helpers;
 using GarageMate.Api.Mapping;
 using GarageMate.Api.Models;
 using Microsoft.EntityFrameworkCore;
@@ -46,23 +47,52 @@ public static class CustomerEndpoints
         })
         .WithName(GetCustomerEndpointName);
 
-        group.MapPost("/", async (CustomerCreateDto newCustomer, GarageMateContext dbContext) =>
+        group.MapPost("/", async (CustomerCreateDto newCustomer, GarageMateContext dbContext, HttpContext http) =>
         {
             if (newCustomer.Type == CustomerType.Individual && newCustomer.Individual is null)
             {
-                return Results.BadRequest("Individual customer details are required.");
+                return ApiProblemHelper.CreateProblem(
+                    "urn:problem-type:missing-customer-subtype-details",
+                    "Missing Required Customer Subtype Details",
+                    StatusCodes.Status400BadRequest,
+                    "Individual customer details are required.",
+                    http.Request.Path
+                );
             }
             if (newCustomer.Type == CustomerType.Company && newCustomer.Company is null)
             {
-                return Results.BadRequest("Company customer details are required.");
+                return ApiProblemHelper.CreateProblem(
+                    "urn:problem-type:missing-customer-subtype-details",
+                    "Missing Required Customer Subtype Details",
+                    StatusCodes.Status400BadRequest,
+                    "Company customer details are required.",
+                    http.Request.Path
+                );
             }
+
+            var contactInfoExists = await dbContext.Customers
+                .AnyAsync(c => c.Email == newCustomer.Email || c.PhoneNumber == newCustomer.PhoneNumber);
+            var validationResult = ValidationHelper.ValidateDuplicateRecord(contactInfoExists, "Contact Detail", http.Request);
+            if (validationResult is not null) return validationResult;
+
+            validationResult = ValidationHelper.ValidateDto(newCustomer, http.Request);
+            if (validationResult is not null) return validationResult;
 
             var customer = newCustomer.ToEntity();
             dbContext.Customers.Add(customer);
-            await dbContext.SaveChangesAsync();
 
-            var dto = customer.ToCustomerDetailsDto();
-            return Results.CreatedAtRoute(GetCustomerEndpointName, new { id = customer.Id }, dto);
+            return await DbContextHelper.TrySaveChangesAsync(
+                dbContext,
+                http,
+                () =>
+                {
+                    return Task.FromResult(Results.CreatedAtRoute(
+                        GetCustomerEndpointName,
+                        new { id = customer.Id },
+                        customer.ToCustomerDetailsDto()
+                    ));
+                }
+            );
         });
 
         group.MapPut("/{id}", async (int id, CustomerDetailsUpdateDto updatedCustomer, GarageMateContext dbContext) =>
