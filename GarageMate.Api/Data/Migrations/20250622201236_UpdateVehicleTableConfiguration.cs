1cfa93190c2dfdfe0cf50514603796628a14a0b1
Ralph Arsy Bauto <rab.devph@gmail.com>
2025-06-23 00:18:35 +0400

    refactor(migrations): update Vehicle table schema and rename to Vehicles

---
+++ GarageMate.Api/Data/Migrations/20250622201236_UpdateVehicleTableConfiguration.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GarageMate.Api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateVehicleTableConfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_VehicleOwnerships_Vehicle_VehicleId",
+                table: "VehicleOwnerships");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Vehicle",
+                table: "Vehicle");
+
+            migrationBuilder.RenameTable(
+                name: "Vehicle",
+                newName: "Vehicles");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Vin",
+                table: "Vehicles",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PlateNumber",
+                table: "Vehicles",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Model",
+                table: "Vehicles",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Make",
+                table: "Vehicles",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Vehicles",
+                table: "Vehicles",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_VehicleOwnerships_Vehicles_VehicleId",
+                table: "VehicleOwnerships",
+                column: "VehicleId",
+                principalTable: "Vehicles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_VehicleOwnerships_Vehicles_VehicleId",
+                table: "VehicleOwnerships");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Vehicles",
+                table: "Vehicles");
+
+            migrationBuilder.RenameTable(
+                name: "Vehicles",
+                newName: "Vehicle");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Vin",
+                table: "Vehicle",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PlateNumber",
+                table: "Vehicle",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Model",
+                table: "Vehicle",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Make",
+                table: "Vehicle",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Vehicle",
+                table: "Vehicle",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_VehicleOwnerships_Vehicle_VehicleId",
+                table: "VehicleOwnerships",
+                column: "VehicleId",
+                principalTable: "Vehicle",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
