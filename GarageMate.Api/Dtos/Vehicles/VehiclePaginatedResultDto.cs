4be49fbbf7c07abd36fa861b805d4a6565f18932
Ralph Arsy Bauto <rab.devph@gmail.com>
2025-06-23 18:03:40 +0400

    feat(dtos): add generic VehiclePaginatedResultDto<T> for paginated responses

---
+++ GarageMate.Api/Dtos/Vehicles/VehiclePaginatedResultDto.cs
@@ -0,0 +1,9 @@
+namespace GarageMate.Api.Dtos.Vehicles;
+
+public record class VehiclePaginatedResultDto<T>
+{
+    public int Page { get; init; }
+    public int PageSize { get; init; }
+    public int TotalCount { get; init; }
+    public List<T> Items { get; init; } = [];
+}
