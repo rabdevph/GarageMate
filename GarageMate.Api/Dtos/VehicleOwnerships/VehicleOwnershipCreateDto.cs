94d0b8d6282295816f61503ca8019a2543584523
Ralph Arsy Bauto <rab.devph@gmail.com>
2025-06-23 18:05:59 +0400

    feat(dtos): add vehicle ownership DTOs for vehicle creation and details views

---
+++ GarageMate.Api/Dtos/VehicleOwnerships/VehicleOwnershipCreateDto.cs
@@ -0,0 +1,9 @@
+namespace GarageMate.Api.Dtos.VehicleOwnerships;
+
+public record class VehicleOwnershipCreateDto
+{
+    public int VehicleId { get; init; }
+    public int CustomerId { get; init; }
+    public bool IsCurrentOwner { get; init; }
+    public string? Notes { get; init; }
+}
---
+++ GarageMate.Api/Dtos/VehicleOwnerships/VehicleOwnershipDetailsDto.cs
@@ -0,0 +1,10 @@
+namespace GarageMate.Api.Dtos.VehicleOwnerships;
+
+public record class VehicleOwnershipDetailsDto
+{
+    public int Id { get; init; }
+    public int VehicleId { get; init; }
+    public int CustomerId { get; init; }
+    public bool IsCurrentOwner { get; init; }
+    public string? Notes { get; init; }
+}
