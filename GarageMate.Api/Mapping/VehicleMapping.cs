cdacff0a6cf617e5cb351d4d1ad7c3a78c2bd06a
Ralph Arsy Bauto <rab.devph@gmail.com>
2025-06-24 13:41:05 +0400

    refactor: move UpdateVehicleDetails to VehicleMapping and delete unused VehicleExtension.cs

--- GarageMate.Api/Mapping/VehicleMapping.cs
+++ GarageMate.Api/Mapping/VehicleMapping.cs
@@ -55,4 +55,15 @@ public static class VehicleMapping
             CurrentOwner = ownerDto
         };
     }
+
+    public static void UpdateVehicleDetails(this Vehicle vehicle, VehicleDetailsUpdateDto dto)
+    {
+        vehicle.PlateNumber = dto.PlateNumber;
+        vehicle.Make = dto.Make;
+        vehicle.Model = dto.Model;
+        vehicle.Year = dto.Year;
+        vehicle.Color = dto.Color;
+        vehicle.Vin = dto.Vin;
+        vehicle.Notes = dto.Notes;
+    }
 }
