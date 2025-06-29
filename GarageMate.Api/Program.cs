26c408eb8feeeef3d721db92edef3fd425125e45
Ralph Arsy Bauto <rab.devph@gmail.com>
2025-06-29 08:27:17 +0400

    chore(config): enable CORS

--- GarageMate.Api/Program.cs
+++ GarageMate.Api/Program.cs
@@ -21,7 +21,12 @@ builder.Services.Configure<JsonOptions>(options =>
     options.SerializerOptions.Converters.Add(new JsonStringEnumConverter(null, allowIntegerValues: false));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
+
+app.UseCors(policy =>
+    policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
 app.MapCustomerEndpoints();
 app.MapVehicleEndpoints();
