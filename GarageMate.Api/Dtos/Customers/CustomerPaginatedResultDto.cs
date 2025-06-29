aa8ad7c233ecbe364cd94f28d0343834ad3cbf2f
Ralph Arsy Bauto <rab.devph@gmail.com>
2025-06-29 08:20:08 +0400

    feat(dtos): add total page count in CustomerPaginatedResultDto

--- GarageMate.Api/Dtos/Customers/CustomerPaginatedResultDto.cs
+++ GarageMate.Api/Dtos/Customers/CustomerPaginatedResultDto.cs
@@ -5,5 +5,6 @@ public record class CustomerPaginatedResultDto<T>
     public int Page { get; init; }
     public int PageSize { get; init; }
     public int TotalCount { get; init; }
+    public int TotalPages { get; init; }
     public List<T> Items { get; init; } = [];
 }
